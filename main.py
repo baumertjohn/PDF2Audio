09812d4ba7b7c22158ca8354d67c3fe534bdc0fc
John Baumert <36886175+baumertjohn@users.noreply.github.com>
2022-03-31 05:55:40 -0600

    First Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# PDF to Audio Project
+# Create a script that takes a PDF file and converts the text to speech.
+
+# Example Text-To-Speech APIs
+# http://www.ispeech.org/api/#introduction
+# https://cloud.google.com/text-to-speech/docs/basics
+# https://aws.amazon.com/polly/
+
+# Import PDF 2 Text
+
+# Define API for conversion
+
+# Check for filename at command line
+# Else ask user for input
+
+# Parse file through PDF 2 text
+
+# Submit file to online API for conversion
+
+# Save or play audio returned
