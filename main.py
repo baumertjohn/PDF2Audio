09f63f22f4429698c243d709df0e12937bda3f46
John Baumert <baumert.john@gmail.com>
2022-04-02 10:48:07 -0600

    Added function to get working file as command line argument or from user input.

--- main.py
+++ main.py
@@ -12,13 +12,45 @@
 
 
 # Import PDF 2 Text
+import pdftotext
+import sys
 
 # Define API for conversion
+# API URL GOES HERE
 
-# Check for filename at command line
-# Else ask user for input
 
-# Parse file through PDF 2 text
+def open_PDF():
+    """Check for a filename added at command line
+    or ask the user to enter a filename."""
+    # Check for command line input
+    try:
+        cl_input = sys.argv[1]
+        with open(cl_input, "rb") as file:
+            pdf = pdftotext.PDF(file)
+            return pdf
+    except IndexError:  # No argument at command line
+        pass
+    except (FileNotFoundError, pdftotext.Error):
+        print("\nFile add at command line not found or not a valid PDF.\n")
+
+    # If no command line input ask user for input
+    while True:
+        work_file = input("What is the file you want to convert?\n> ")
+        try:
+            with open(work_file, "rb") as file:
+                pdf = pdftotext.PDF(file)
+                return pdf
+        except (FileNotFoundError, pdftotext.Error):
+            print("\nFile not found or not valid, try again.\n")
+
+
+pdf = open_PDF()
+
+# Parse file through PDF To Text
+text_to_convert = f"".join(pdf)
+
+print(text_to_convert)
+
 
 # Submit file to online API for conversion
 
